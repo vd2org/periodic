ec7e7e8eb38f92c801d463935e4f7aa5c7eae472
Rocket <rocket@>
2019-02-21 16:06:08 +0200

    Some code cleanup.

--- periodic.py
+++ periodic.py
@@ -1,8 +1,5 @@
-
-import sys
-import traceback
-import logging
 import asyncio
+import logging
 from contextlib import suppress
 
 logger = logging.getLogger('periodic')
@@ -15,51 +12,58 @@ class Periodic:
         self.kwargs = kwargs
         self.interval = interval
         self.is_started = False
-        
+
         self._is_running = False
         self._loop = loop
         self._task = None
         self._handler = None
- 
+
     async def start(self, immediately=False):
-        if not self.is_started:
-            self.is_started = True
-            self._is_running = False
-            if immediately:
-                self._handler = self._loop.call_soon(self._run)
-            else:
-                self._handler = self._loop.call_later(self.interval, self._run)
+        if self.is_started:
+            return
+
+        self.is_started = True
+        self._is_running = False
+        if immediately:
+            self._handler = self._loop.call_soon(self._run)
+        else:
+            self._handler = self._loop.call_later(self.interval, self._run)
 
     async def stop(self):
-        if self.is_started:
-            self.is_started = False
-            if self._handler:
-                self._handler.cancel()
-                self._handler = None
-            if self._task:
-                self._task.cancel()
-                with suppress(asyncio.CancelledError):
-                    await self._task
-                self._task = None
+        if not self.is_started:
+            return
+
+        self.is_started = False
+        if self._handler:
+            self._handler.cancel()
+            self._handler = None
+        if self._task:
+            self._task.cancel()
+            with suppress(asyncio.CancelledError):
+                await self._task
+            self._task = None
 
     async def _runner(self):
+        if not self.is_started:
+            return
+
         try:
-            if self.is_started:
-                with suppress(asyncio.CancelledError):
-                    await self.coro(*self.args, **self.kwargs)
+            with suppress(asyncio.CancelledError):
+                await self.coro(*self.args, **self.kwargs)
         except:
-            logger.exception('Got exception durning awaiting periodicly')
+            logger.exception('Got exception during awaiting periodically')
         finally:
             self._is_running = False
-                
+
     def _run(self):
         if not self.is_started:
             return
 
         self._handler = self._loop.call_later(self.interval, self._run)
-        
-        if not self._is_running:
-            self._is_running = True
-            self._task = asyncio.ensure_future(self._runner())
-        else:
-            logger.error('Trottling periodic call')
+
+        if self._is_running:
+            logger.error('Throttling periodic call')
+            return
+
+        self._is_running = True
+        self._task = asyncio.create_task(self._runner())
